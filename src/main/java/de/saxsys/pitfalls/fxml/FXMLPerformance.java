5a23b4175a639dfe9f2674b71da5c07ebd302fd7
Alexander Casall <alexander.casall@gmail.com>
2015-10-13 11:42:58 +0200

    Sysos to FXML Performance Example

--- src/main/java/de/saxsys/pitfalls/fxml/FXMLPerformance.java
+++ src/main/java/de/saxsys/pitfalls/fxml/FXMLPerformance.java
@@ -7,37 +7,35 @@ import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 
 public class FXMLPerformance extends Application {
-
-    public static void main(String[] args) throws IOException {
-        launch(args);
-    }
-
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        loadWithWildcards();
-        loadWithoutWildcards();
-
-        loadWithWildcards();
-        loadWithoutWildcards();
-
-        loadWithWildcards();
-        loadWithoutWildcards();
-    }
-
-    private void loadWithoutWildcards() throws IOException {
-        long startTime = System.currentTimeMillis();
-        FXMLLoader loader = new FXMLLoader(FXMLPerformance.class.getResource("WithoutWildcards.fxml"));
-        loader.load();
-        long estimatedTime = System.currentTimeMillis() - startTime;
-        System.out.println(estimatedTime);
-    }
-
-    private void loadWithWildcards() throws IOException {
-        long startTime = System.currentTimeMillis();
-        FXMLLoader loader = new FXMLLoader(FXMLPerformance.class.getResource("WithWildcards.fxml"));
-        loader.load();
-        long estimatedTime = System.currentTimeMillis() - startTime;
-        System.out.println(estimatedTime);
-    }
-
+	
+	public static void main(String[] args) throws IOException {
+		launch(args);
+	}
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		
+		for (int i = 0; i < 5; i++) {
+			System.out.println("Loading Iteration###### " + i);
+			loadWithWildcards();
+			loadWithoutWildcards();
+		}
+	}
+	
+	private void loadWithoutWildcards() throws IOException {
+		long startTime = System.currentTimeMillis();
+		FXMLLoader loader = new FXMLLoader(FXMLPerformance.class.getResource("WithoutWildcards.fxml"));
+		loader.load();
+		long estimatedTime = System.currentTimeMillis() - startTime;
+		System.out.println("Without Wildcards: \t" + estimatedTime);
+	}
+	
+	private void loadWithWildcards() throws IOException {
+		long startTime = System.currentTimeMillis();
+		FXMLLoader loader = new FXMLLoader(FXMLPerformance.class.getResource("WithWildcards.fxml"));
+		loader.load();
+		long estimatedTime = System.currentTimeMillis() - startTime;
+		System.out.println("With Wildcards: \t" + estimatedTime);
+	}
+	
 }
