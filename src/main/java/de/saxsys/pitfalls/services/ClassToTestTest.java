5139be2e93caa428df458ff35ed9e1e48d0c8b6d
Alexander Casall <alexander.casall@gmail.com>
2015-10-13 09:31:48 +0200

    Added test

--- src/main/java/de/saxsys/pitfalls/services/ClassToTestTest.java
+++ src/main/java/de/saxsys/pitfalls/services/ClassToTestTest.java
@@ -9,6 +9,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import de.saxsys.javafx.test.JfxRunner;
+import de.saxsys.javafx.test.TestInJfxThread;
 
 /**
  * A test for {@link ClassToTest} using the solution pattern given by
@@ -16,33 +17,49 @@ import de.saxsys.javafx.test.JfxRunner;
  */
 @RunWith(JfxRunner.class)
 public class ClassToTestTest {
-
-    private final ClassToTest cut = new ClassToTest();
-
-    @Test(timeout = 6000)
-    public void testLongLastingOperationCallingTwice() throws ExecutionException, InterruptedException {
-
-        // first call succeeds
-        myTest();
-
-    }
-
-    private void myTest() throws InterruptedException, ExecutionException {
-        CompletableFuture<String> future = new CompletableFuture<>();
-
-        cut.valueProperty().addListener((b, o, n) -> {
-            System.out.println(n);
-            if (n != null) {
-                future.complete(n);
-            }
-        });
-
-        // STARTET ONCE....
-        cut.start();
-
-        // EXC
-        cut.valueProperty();
-        // assertEquals("I'm an expensive result", cut.getValue());
-        assertEquals("I'm an expensive result.", future.get());
-    }
+	
+	
+	@Test(timeout = 6000)
+	public void testLongLastingOperation() throws ExecutionException, InterruptedException {
+		ClassToTest cut = new ClassToTest();
+		
+		CompletableFuture<String> future = new CompletableFuture<>();
+		
+		cut.valueProperty().addListener((b, o, n) -> {
+			if (n != null) {
+				future.complete(n);
+			}
+		});
+		
+		// STARTET ONCE....
+		cut.start();
+		
+		// EXC
+		System.out.println(cut.valueProperty());
+		// assertEquals("I'm an expensive result", cut.getValue());
+		assertEquals("I'm an expensive result.", future.get());
+	}
+	
+	@Test(timeout = 6000)
+	@TestInJfxThread
+	public void testLongLastingOperationInFXThread() throws ExecutionException, InterruptedException {
+		
+		ClassToTest cut = new ClassToTest();
+		
+		CompletableFuture<String> future = new CompletableFuture<>();
+		
+		cut.valueProperty().addListener((b, o, n) -> {
+			if (n != null) {
+				future.complete(n);
+			}
+		});
+		
+		// STARTET ONCE....
+		cut.start();
+		
+		// EXC
+		// cut.valueProperty();
+		// assertEquals("I'm an expensive result", cut.getValue());
+		assertEquals("I'm an expensive result.", future.get());
+	}
 }
