992cc945e89d51c0c2f0c4d692446fa37d9bfcb8
amoAHCP <amo.ahcp@gmail.com>
2015-10-13 11:26:59 +0200

    add unitTest methods

--- src/main/java/de/saxsys/pitfalls/services/ClassToTestTest.java
+++ src/main/java/de/saxsys/pitfalls/services/ClassToTestTest.java
@@ -1,15 +1,15 @@
 package de.saxsys.pitfalls.services;
 
-import static org.junit.Assert.assertEquals;
+import de.saxsys.javafx.test.JfxRunner;
+import de.saxsys.javafx.test.TestInJfxThread;
+import org.jacpfx.concurrency.FXWorker;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
-import de.saxsys.javafx.test.JfxRunner;
-import de.saxsys.javafx.test.TestInJfxThread;
+import static org.junit.Assert.assertEquals;
 
 /**
  * A test for {@link ClassToTest} using the solution pattern given by
@@ -38,6 +38,41 @@ public class ClassToTestTest {
 		System.out.println(cut.valueProperty());
 		// assertEquals("I'm an expensive result", cut.getValue());
 		assertEquals("I'm an expensive result.", future.get());
+	}
+
+	@Test(timeout = 6000)
+	public void testLongLastingOperationWithFXWorker() throws ExecutionException, InterruptedException {
+
+		final FXWorker<?> handler = FXWorker.instance();
+
+		handler.supplyOnFXThread(()-> {
+			ClassToTest cut = new ClassToTest();
+			cut.start();
+			return cut;
+		}).functionOnFXThread((cut)->{
+			assertEquals("I'm an expensive result", cut.getValue());
+			System.out.println(cut.valueProperty());
+			return cut.getValue();
+		}).execute(val->assertEquals("I'm an expensive result.", val));
+		assertEquals(true,true);
+	}
+
+	@Test(timeout = 6000)
+	public void testLongLastingOperationWithFXWorker2() throws ExecutionException, InterruptedException {
+
+		final FXWorker<?> handler = FXWorker.instance();
+
+		handler.supplyOnExecutorThread(() -> {
+			ClassToTestService cut = new ClassToTestService();
+			String result = cut.longRunning();
+			handler.updateMessage(result);
+			return result;
+		}).functionOnFXThread((cut)->{
+			assertEquals("I'm an expensive result", cut);
+			assertEquals("I'm an expensive result", handler.messageProperty().getValue());
+			return cut;
+		}).execute(val->assertEquals("I'm an expensive result.", val));
+		assertEquals(true,true);
 	}
 	
 	@Test(timeout = 6000)
